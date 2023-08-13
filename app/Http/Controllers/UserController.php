eb60dfc7509fc6c7de89d4dce24832621751b102
Hudzaifah <hudz1357@gmail.com>
2023-08-14 02:04:57 +0700

    14-8-2023

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index() {
-        return view('pages.admin.users.index');
+        $users = User::get();
+
+        return view('pages.admin.users.index', compact('users'));
     }
 
     public function create() {
         return view('pages.admin.users.create');
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users',
+            'role' => 'required|in:admin,teacher,user',
+            'password' => 'required|confirmed',
+        ]);
+
+        $validated['password'] = Hash::make($request->password);
+
+        User::create($validated);
+
+        return redirect()->route('admin.user.index');
     }
 
     public function show($id) {
@@ -19,10 +38,20 @@ class UserController extends Controller
     }
 
     public function edit($id) {
-        return view('pages.admin.users.edit');
+        $user = User::find($id);
+
+        return view('pages.admin.users.edit', compact('user'));
     }
 
     public function change_password() {
         return view('pages.admin.change_password');
+    }
+
+    public function destroy($id) {
+        $user = User::find($id);
+
+        $user->delete();
+
+        return redirect()->route('admin.user.index');
     }
 }
--- resources/views/pages/admin/users/index.blade.php
+++ resources/views/pages/admin/users/index.blade.php
@@ -103,34 +103,40 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="w-4 p-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-1" name="chk" type="checkbox"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Hudzaifah
-                            </th>
-                            <td class="px-6 py-4">
-                                hudz@gmail.com
-                            </td>
-                            <td class="px-6 py-4">
-                                Admin
-                            </td>
-                            <td class="flex items-center px-6 py-4 space-x-3">
-                                <a href="{{ route('admin.user.show', 1) }}"
-                                    class="font-medium text-green-600 dark:text-green-500 hover:underline">Detail</a>
-                                <a href="{{ route('admin.user.edit', 1) }}"
-                                    class="font-medium text-yellow-600 dark:text-yellow-500 hover:underline">Edit</a>
-                                <a href="#"
-                                    class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
-                            </td>
-                        </tr>
+                        @foreach ($users as $data)
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        <input id="checkbox-table-search-1" name="chk" type="checkbox"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $data->name }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $data->email }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $data->role }}
+                                </td>
+                                <td class="flex items-center px-6 py-4 space-x-3">
+                                    <a href="{{ route('admin.user.show', $data->id) }}"
+                                        class="font-medium text-green-600 dark:text-green-500 hover:underline">Detail</a>
+                                    <a href="{{ route('admin.user.edit', $data->id) }}"
+                                        class="font-medium text-yellow-600 dark:text-yellow-500 hover:underline">Edit</a>
+                                    <form action="{{ route('admin.user.destroy', $data->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button onclick="confirm('Anda yakin ingin menghapus data?')"
+                                            class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
